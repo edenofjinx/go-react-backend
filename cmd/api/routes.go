682cd552741eabca40f1b6589d42131f408c553c
Julius Liaudanskis <julius.l@emotion.lt>
2021-12-06 16:03:00 +0200

    go-react: 89

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,14 +19,13 @@ func (app *application) routes() http.Handler {
 	secure := alice.New(app.checkToken)
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/graphql/list", app.moviesGraphql)
 	router.HandlerFunc(http.MethodPost, "/v1/signin", app.Signin)
 	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.GetAllGenres)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.GetAllMoviesByGenre)
 	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editMovie)))
-	//router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
 	router.GET("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovie)))
-	//router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 	return app.enableCORS(router)
 }
